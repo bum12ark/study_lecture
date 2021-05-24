e87fb9fa1ed147f4bf465aac5c3aeb48db280c84
SangBum <bum12ark@gmail.com>
2021-05-24 23:38:47 +0900

    주문 도메인 개발 - 주문 서비스 개발 - 주문 기능 테스트 - 주문 검색 기능 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.aspectj.weaver.ast.Or;
 
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
