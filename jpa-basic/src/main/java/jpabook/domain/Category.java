92776a3c6422dfc851229464f2c0eec2056ab3c1
bum12ark <bum12ark@gmail.com>
2021-10-13 14:29:53 +0900

    실전예제 3 - 다양한 연관관계 매핑

---
+++ jpa-basic/src/main/java/jpabook/domain/Category.java
@@ -0,0 +1,28 @@
+package jpabook.domain;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "PARENT_ID")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "CATEGORY_ITEM",
+            joinColumns = @JoinColumn(name = "CATEGORY_ID"),
+            inverseJoinColumns = @JoinColumn(name = "ITEM_ID")
+    ) // 실습을 위한 다대다 매핑 (실전에서 사용 X)
+    private List<Item> items = new ArrayList<>();
+}
