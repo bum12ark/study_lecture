5590a96b72a2583c7e74ec4f31642b9575228fbe
bum12ark <bum12ark@gmail.com>
2021-10-13 17:59:02 +0900

    상속관계 매핑 - @MappedSuperclass

--- jpa-basic/src/main/java/relationship/mapping/inheritance/domain/Item.java
+++ jpa-basic/src/main/java/relationship/mapping/inheritance/domain/Item.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn
-public class Item {
+public abstract class Item extends BaseEntity {
     @Id @GeneratedValue
     private Long id;
     private String name;
