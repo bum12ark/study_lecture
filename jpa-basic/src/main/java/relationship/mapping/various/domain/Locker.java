f44704bf039842b7b6431aae0ad55c1569e8cca6
bum12ark <bum12ark@gmail.com>
2021-10-13 10:53:12 +0900

    다양한 연관관계 매핑 - 일대일 관계

---
+++ jpa-basic/src/main/java/relationship/mapping/various/domain/Locker.java
@@ -0,0 +1,16 @@
+package relationship.mapping.various.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Locker {
+
+    @Id @GeneratedValue
+    @Column(name = "LOCKER_ID")
+    private Long id;
+    private String name;
+
+    @OneToOne(mappedBy = "locker")
+    private Member member;
+
+}
