0b3c69c04534d4d358e5d5421f5fa13d61574641
bum12ark <bum12ark@gmail.com>
2021-11-18 01:50:57 +0900

    Catalogs Microservice 프로젝트 생성

---
+++ MSA/catalog-service/src/main/java/com/example/catalogservice/entity/Catalog.java
@@ -0,0 +1,29 @@
+package com.example.catalogservice.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Catalog implements Serializable {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+    @Column(nullable = false)
+    private String productName;
+    @Column(nullable = false)
+    private Integer stock;
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+    @Column(nullable = false, updatable = false, insertable = false)
+    private LocalDateTime createdAt;
+}
